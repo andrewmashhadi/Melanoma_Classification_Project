4a806bffa4dd80937c2ba908cff2a907e7e4aab0
Andrew Mashhadi <amiroandy7@gmail.com>
2023-01-04 01:26:51 -0800

    added batch job scripts

---
+++ batch_jobs/cnn2_gpu_train.sh
@@ -0,0 +1,38 @@
+#### submit_job.sh START ####
+#!/bin/bash
+#$ -cwd
+# error = Merged with joblog
+#$ -o joblog.$JOB_ID
+#$ -j y
+## Edit the line below as needed:
+#$ -l h_rt=24:00:00,h_data=16G,gpu
+## Modify the parallel environment
+## and the number of cores as needed:
+#$ -pe shared 1
+# Email address to notify
+#$ -M $USER@mail
+# Notify when
+#$ -m bea
+
+# echo job info on joblog:
+echo "Job $JOB_ID started on:   " `hostname -s`
+echo "Job $JOB_ID started on:   " `date `
+echo " "
+
+# load the job environment:
+. /u/local/Modules/default/init/modules.sh
+
+## Edit the line below as needed:
+module load cuda/10.2
+module load anaconda3
+
+## substitute the command to run your code
+## in the two lines below:
+conda activate MAS_pt
+python cnn2_train_only.py
+
+# echo job info on joblog:
+echo "Job $JOB_ID ended on:   " `hostname -s`
+echo "Job $JOB_ID ended on:   " `date `
+echo " "
+#### submit_job.sh STOP ####
