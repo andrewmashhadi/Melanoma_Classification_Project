e6966e36c986ce7029ae72edf412e583d0a0483d
Andrew Mashhadi <amiroandy7@gmail.com>
2023-01-11 10:42:40 -0800

    tried making smaller batch sizes to see if it helps with being selected, also had a memory problem. I beleive the p4 gpu was chosen.

--- batch_jobs/resnet50_cpu_train.sh
+++ batch_jobs/resnet50_cpu_train.sh
@@ -5,10 +5,10 @@
 #$ -o joblog.$JOB_ID
 #$ -j y
 ## Edit the line below as needed:
-#$ -l h_rt=5:00:00,h_data=16G,gpu,RTX2080Ti
+#$ -l h_rt=24:00:00,h_data=32G
 ## Modify the parallel environment
 ## and the number of cores as needed:
-#$ -pe shared 1
+#$ -pe shared 4
 # Email address to notify
 #$ -M $USER@mail
 # Notify when
@@ -23,13 +23,12 @@ echo " "
 . /u/local/Modules/default/init/modules.sh
 
 ## Edit the line below as needed:
-module load cuda/10.2
 module load anaconda3
 
 ## substitute the command to run your code
 ## in the two lines below:
 conda activate MAS_pt
-python cnn2_train_only.py
+python resnet50_train_only.py
 
 # echo job info on joblog:
 echo "Job $JOB_ID ended on:   " `hostname -s`
